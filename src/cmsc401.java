d36eed89c11efbe18dc6ab63fbc3b9069bc0bea2
davidf <ferrufinoda2@vcu.edu>
2022-11-02 21:31:12 -0400

    Initial commit

---
+++ src/cmsc401.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class cmsc401 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        Scanner in = new Scanner(System.in);
+        //getting then no of cities
+        int numCities = in.nextInt();
+        int numHighways = in.nextInt();
+
+    }
+}
